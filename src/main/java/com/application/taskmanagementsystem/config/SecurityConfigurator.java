4cde934e8def1a321274addbfd376cc8fae142b6
BlackDaddy220232 <sashamynzul@gmail.com>
2024-08-16 22:24:00 +0300

    feat: SecurityController, SecurityConfigurator

---
+++ src/main/java/com/application/taskmanagementsystem/config/SecurityConfigurator.java
@@ -0,0 +1,108 @@
+package com.application.taskmanagementsystem.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.application.taskmanagementsystem.security.TokenFilter;
+import com.application.taskmanagementsystem.service.UserService;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+@Data
+public class SecurityConfigurator {
+  private TokenFilter tokenFilter;
+  private UserService userService;
+  private MyAuthenticationEntryPoint authenticationEntryPoint;
+
+  @Autowired
+  public void setMyAuthenticationEntryPoint(MyAuthenticationEntryPoint authenticationEntryPoint) {
+    this.authenticationEntryPoint = authenticationEntryPoint;
+  }
+
+  @Autowired
+  public void setUserService(UserService userService) {
+    this.userService = userService;
+  }
+
+  @Autowired
+  public void setTokenFilter(TokenFilter tokenFilter) {
+    this.tokenFilter = tokenFilter;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  @Primary
+  public AuthenticationManagerBuilder configureAuthenticationManagerBuilder(
+      AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    return authenticationManagerBuilder;
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .cors(
+            cors ->
+                cors.configurationSource(
+                    request -> {
+                      CorsConfiguration configuration = new CorsConfiguration();
+                      configuration.setAllowedOrigins(
+                          List.of(
+                              "http://localhost:8080",
+                              "http://localhost:3000/",
+                              "http://localhost:3000/stats"));
+                      configuration.setAllowedMethods(
+                          Arrays.asList("GET", "POST", "PUT", "DELETE")); // Разрешенные методы
+                      configuration.setAllowedHeaders(
+                          Arrays.asList("Authorization", "Content-Type")); // Разрешенные заголовки
+                      configuration.setAllowCredentials(true); // Разрешение использования куки
+                      return configuration;
+                    }))
+        .exceptionHandling(
+            exceptions -> exceptions.authenticationEntryPoint(authenticationEntryPoint))
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers("/auth/**", "/users/getAllUsers")
+                    .permitAll()
+                    .requestMatchers(
+                        "/countries/**",
+                        "/users/deleteUser",
+                        "/users/getUserByUsername",
+                        "/users/addCity")
+                    .fullyAuthenticated()
+                    .anyRequest()
+                    .permitAll())
+        .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
+}
---
+++ src/main/java/com/application/taskmanagementsystem/controller/SecurityController.java
@@ -0,0 +1,46 @@
+package com.application.taskmanagementsystem.controller;
+
+import com.application.taskmanagementsystem.model.dto.PasswordRequest;
+import com.application.taskmanagementsystem.model.dto.SignInRequest;
+import com.application.taskmanagementsystem.model.dto.SignUpRequest;
+import com.application.taskmanagementsystem.service.SecurityService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class SecurityController {
+  private SecurityService securityService;
+
+  @Autowired
+  public void setSecurityService(SecurityService securityService) {
+    this.securityService = securityService;
+  }
+
+  @PostMapping("/signup")
+  ResponseEntity<String> signup(@RequestBody SignUpRequest signUpRequest) {
+    return ResponseEntity.ok(securityService.register(signUpRequest));
+  }
+
+  @PostMapping("/signin")
+  public String signin(@RequestBody SignInRequest signInRequest, HttpServletResponse response) {
+    String token = securityService.login(signInRequest);
+    Cookie cookie = new Cookie("token", token);
+    cookie.setHttpOnly(true);
+    cookie.setSecure(true);
+    cookie.setPath("/");
+    response.addCookie(cookie);
+    return token; // Delete this in relies version
+  }
+
+  @PatchMapping("/editPassword")
+  public ResponseEntity<String> editPassword(
+          @RequestBody PasswordRequest passwordRequest, HttpServletRequest request) {
+    securityService.changePassword(passwordRequest, request);
+    return ResponseEntity.ok("Password was successfully updated");
+  }
+}

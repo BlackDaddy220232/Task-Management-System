29b719363b5a0f63c2827629c4a39900cb715e67
BlackDaddy220232 <sashamynzul@gmail.com>
2024-08-19 14:29:25 +0300

    code refactoring

--- src/test/java/com/application/taskmanagementsystem/TaskManagementSystemApplicationTests.java
+++ src/test/java/com/application/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -7,6 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TaskManagementSystemApplicationTests {
 	@Test
 	void contextLoads() {
+		/*
+		 * This test method is intentionally left empty.
+		 * The purpose of this test is to verify that the Spring application context
+		 * can be successfully loaded and initialized. Since there are no specific
+		 * assertions to be made, the method body is left empty.
+		 * If the context fails to load, the test will fail, indicating an issue
+		 * with the application configuration.
+		 */
 	}
 
 }
